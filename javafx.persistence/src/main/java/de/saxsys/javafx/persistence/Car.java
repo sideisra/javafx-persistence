0755726cb84d99eb9eb9e6416a1063b720b0614a
Denny Israel <denny.israel@gmail.com>
2015-10-13 22:22:22 +0200

    code formated

--- javafx.persistence/src/main/java/de/saxsys/javafx/persistence/Car.java
+++ javafx.persistence/src/main/java/de/saxsys/javafx/persistence/Car.java
@@ -12,33 +12,33 @@ import javafx.beans.property.StringProperty;
 @XmlRootElement(name = "car")
 public class Car {
 
-	private final StringProperty manufacturer = new SimpleStringProperty();
-	private final StringProperty model = new SimpleStringProperty();
+ private final StringProperty manufacturer = new SimpleStringProperty();
+ private final StringProperty model = new SimpleStringProperty();
 
-	public final StringProperty manufacturerProperty() {
-		return this.manufacturer;
-	}
+ public final StringProperty manufacturerProperty() {
+  return this.manufacturer;
+ }
 
-	@XmlAttribute
-	public final String getManufacturer() {
-		return this.manufacturerProperty().get();
-	}
+ @XmlAttribute
+ public final String getManufacturer() {
+  return this.manufacturerProperty().get();
+ }
 
-	public final void setManufacturer(final String manufacturer) {
-		this.manufacturerProperty().set(manufacturer);
-	}
+ public final void setManufacturer(final String manufacturer) {
+  this.manufacturerProperty().set(manufacturer);
+ }
 
-	public final StringProperty modelProperty() {
-		return this.model;
-	}
+ public final StringProperty modelProperty() {
+  return this.model;
+ }
 
-	@XmlAttribute
-	public final String getModel() {
-		return this.modelProperty().get();
-	}
+ @XmlAttribute
+ public final String getModel() {
+  return this.modelProperty().get();
+ }
 
-	public final void setModel(final String model) {
-		this.modelProperty().set(model);
-	}
+ public final void setModel(final String model) {
+  this.modelProperty().set(model);
+ }
 
 }
