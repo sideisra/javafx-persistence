5ce14f7249db91a439cd6a2fa110797763a14aa4
Denny Israel <denny.israel@saxsys.de>
2016-01-04 10:17:34 +0100

    async initial loading of data

--- javafx.persistence/src/main/java/de/saxsys/javafx/persistence/CarView.java
+++ javafx.persistence/src/main/java/de/saxsys/javafx/persistence/CarView.java
@@ -1,9 +1,15 @@
 package de.saxsys.javafx.persistence;
 
+import java.util.List;
+
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,107 +19,138 @@ import javafx.scene.control.cell.TextFieldListCell;
 import javafx.util.StringConverter;
 
 public class CarView {
- @FXML
- private ListView<Manufacturer> lvManufacturer;
+  @FXML
+  private ListView<Manufacturer> lvManufacturer;
 
- @FXML
- private Button btnAddManufacturer;
+  @FXML
+  private Button btnAddManufacturer;
 
- @FXML
- private Button btnRemoveManufacturer;
+  @FXML
+  private Button btnRemoveManufacturer;
 
- @FXML
- private ListView<Car> lvCars;
+  @FXML
+  private ListView<Car> lvCars;
 
- @FXML
- private Button btnAddCar;
+  @FXML
+  private Button btnAddCar;
 
- @FXML
- private Button btnRemove;
+  @FXML
+  private Button btnRemoveCar;
 
- @FXML
- private TextField txtNewManufacturerName;
- @FXML
- private TextField txtNewCarName;
+  @FXML
+  private TextField txtNewManufacturerName;
+  @FXML
+  private TextField txtNewCarName;
 
- private final CarDb carDb = new CarDb();
+  private final CarDb carDb = new CarDb();
 
- private final ListProperty<Manufacturer> manufacturers = new SimpleListProperty<>(FXCollections.observableArrayList());
+  private final BooleanProperty loading = new SimpleBooleanProperty();
 
- @FXML
- private void initialize() {
-  lvCars.setCellFactory(param -> new TextFieldListCell<Car>(new StringConverter<Car>() {
+  private final ListProperty<Manufacturer> manufacturers = new SimpleListProperty<>(
+      FXCollections.observableArrayList());
 
-   @Override
-   public String toString(final Car object) {
-    return object.getName();
-   }
+  @FXML
+  private void initialize() {
+    btnAddCar.disableProperty().bind(loading);
+    btnAddManufacturer.disableProperty().bind(loading);
+    btnRemoveCar.disableProperty().bind(loading);
+    btnRemoveManufacturer.disableProperty().bind(loading);
 
-   @Override
-   public Car fromString(final String string) {
-    throw new RuntimeException("should not happen");
-   }
+    lvCars.setCellFactory(param -> new TextFieldListCell<Car>(new StringConverter<Car>() {
 
-  }));
-  lvManufacturer.setCellFactory(param -> new TextFieldListCell<Manufacturer>(new StringConverter<Manufacturer>() {
+      @Override
+      public String toString(final Car object) {
+        return object.getName();
+      }
 
-   @Override
-   public String toString(final Manufacturer object) {
-    return object.getName();
-   }
+      @Override
+      public Car fromString(final String string) {
+        throw new RuntimeException("should not happen");
+      }
 
-   @Override
-   public Manufacturer fromString(final String string) {
-    throw new RuntimeException("should not happen");
-   }
-  }));
-  lvManufacturer.setItems(manufacturers);
-  lvManufacturer.getSelectionModel().selectedItemProperty()
-    .addListener((ChangeListener<Manufacturer>) (observable, oldValue, newValue) -> {
-     lvCars.setItems(newValue.carsProperty());
+    }));
+    lvManufacturer.setCellFactory(param -> new TextFieldListCell<Manufacturer>(new StringConverter<Manufacturer>() {
+
+      @Override
+      public String toString(final Manufacturer object) {
+        return object.getName();
+      }
+
+      @Override
+      public Manufacturer fromString(final String string) {
+        throw new RuntimeException("should not happen");
+      }
+    }));
+    lvManufacturer.setItems(manufacturers);
+    lvManufacturer.getSelectionModel().selectedItemProperty()
+        .addListener((ChangeListener<Manufacturer>) (observable, oldValue, newValue) -> {
+          lvCars.setItems(newValue.carsProperty());
+        });
+    loadManufacturers();
+  }
+
+  private void loadManufacturers() {
+    final Service<List<Manufacturer>> service = new Service<List<Manufacturer>>() {
+      @Override
+      protected Task<List<Manufacturer>> createTask() {
+        return new Task<List<Manufacturer>>() {
+
+          @Override
+          protected List<Manufacturer> call() throws Exception {
+            System.out.println("loading manufacturer in " + Thread.currentThread().getName());
+            return carDb.readManufacturer();
+          }
+        };
+      }
+    };
+    service.setOnSucceeded(event -> {
+      System.out.println("setting loaded manufacturers in " + Thread.currentThread().getName());
+      manufacturers.addAll(service.getValue());
+      loading.set(false);
     });
-  manufacturers.addAll(carDb.readManufacturer());
- }
-
- @FXML
- void addCar(final ActionEvent event) {
-  final String newCarName = txtNewCarName.getText();
-  if (newCarName != null && !newCarName.isEmpty()) {
-   final Manufacturer selectedManufacturer = lvManufacturer.getSelectionModel().getSelectedItem();
-   if (selectedManufacturer != null) {
-    selectedManufacturer.carsProperty().add(new Car(newCarName));
-    carDb.saveManufacturer(selectedManufacturer);
-   }
+    loading.set(true);
+    service.start();
   }
- }
 
- @FXML
- void removeCar(final ActionEvent event) {
-  final Manufacturer selectedManufacturer = lvManufacturer.getSelectionModel().getSelectedItem();
-  final Car selectedCar = lvCars.getSelectionModel().getSelectedItem();
-  if (selectedManufacturer != null && selectedCar != null) {
-   selectedManufacturer.carsProperty().remove(selectedCar);
-   carDb.saveManufacturer(selectedManufacturer);
+  @FXML
+  void addCar(final ActionEvent event) {
+    final String newCarName = txtNewCarName.getText();
+    if (newCarName != null && !newCarName.isEmpty()) {
+      final Manufacturer selectedManufacturer = lvManufacturer.getSelectionModel().getSelectedItem();
+      if (selectedManufacturer != null) {
+        selectedManufacturer.carsProperty().add(new Car(newCarName));
+        carDb.saveManufacturer(selectedManufacturer);
+      }
+    }
   }
- }
 
- @FXML
- void addManufacturer(final ActionEvent event) {
-  final String newManufacturerName = txtNewManufacturerName.getText();
-  if (newManufacturerName != null && !newManufacturerName.isEmpty()) {
-   final Manufacturer newManufacturer = new Manufacturer(newManufacturerName);
-   manufacturers.add(newManufacturer);
-   carDb.saveManufacturer(newManufacturer);
+  @FXML
+  void removeCar(final ActionEvent event) {
+    final Manufacturer selectedManufacturer = lvManufacturer.getSelectionModel().getSelectedItem();
+    final Car selectedCar = lvCars.getSelectionModel().getSelectedItem();
+    if (selectedManufacturer != null && selectedCar != null) {
+      selectedManufacturer.carsProperty().remove(selectedCar);
+      carDb.saveManufacturer(selectedManufacturer);
+    }
   }
- }
 
- @FXML
- void removeManufacturer(final ActionEvent event) {
-  final Manufacturer selectedItem = lvManufacturer.getSelectionModel().getSelectedItem();
-  if (selectedItem != null) {
-   manufacturers.remove(selectedItem);
-   carDb.deleteManufacturer(selectedItem);
+  @FXML
+  void addManufacturer(final ActionEvent event) {
+    final String newManufacturerName = txtNewManufacturerName.getText();
+    if (newManufacturerName != null && !newManufacturerName.isEmpty()) {
+      final Manufacturer newManufacturer = new Manufacturer(newManufacturerName);
+      manufacturers.add(newManufacturer);
+      carDb.saveManufacturer(newManufacturer);
+    }
   }
- }
+
+  @FXML
+  void removeManufacturer(final ActionEvent event) {
+    final Manufacturer selectedItem = lvManufacturer.getSelectionModel().getSelectedItem();
+    if (selectedItem != null) {
+      manufacturers.remove(selectedItem);
+      carDb.deleteManufacturer(selectedItem);
+    }
+  }
 
 }
